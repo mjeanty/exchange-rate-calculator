636f6ae3621c470f39f66cf6fa8b0af920278f39
maxjeanty <maxjeanty@gmail.com>
2023-10-22 23:18:30 -0500

    added fetch  for one currency

--- script.js
+++ script.js
@@ -11,8 +11,27 @@ const rateEl = document.getElementById('rate')
 const swap = document.getElementById('swap')
 
 
-//fetch 
+//fetch exchange rate and update DOM
 
-function calculate() {}
+function calculate() {
+    const currency_one =currencyEl_one
+.value
+    const currency_two =currencyEl_two
+.value
+    fetch(`https://v6.exchangerate-api.com/v6/0f0c85dbd08079992085a912/latest/${currency_one}`)
+    .then(res=> res.json())
+    .then(data =>{
+        const rate = data.rates
+    })
+}
+
+//event listener
+currencyEl_one.addEventListener('change', calculate)
+amountEl_one.addEventListener('input', calculate)
+currencyEl_two.addEventListener('change', calculate)
+amountEl_two.addEventListener('input', calculate)
+
+
+
 
 calculate()
