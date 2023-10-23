1a73dc4048a09807e4cdf3a8ba22ecd6f811ce7a
maxjeanty <maxjeanty@gmail.com>
2023-10-22 22:49:39 -0500

    working on JS and fetch

---
+++ script.js
@@ -0,0 +1,18 @@
+const currencyEl_one = document.getElementById
+('currency-one')
+const amountEl_one = document.getElementById
+('currency-one')
+const currencyEl_two = document.getElementById
+('currency-two')
+const amountEl_two = document.getElementById
+('currency-two')
+
+const rateEl = document.getElementById('rate')
+const swap = document.getElementById('swap')
+
+
+//fetch 
+
+function calculate() {}
+
+calculate()
